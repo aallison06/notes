e57844677d547131f0af378c6d92f72ee4636315
aallison06 <131303295+aallison06@users.noreply.github.com>
2023-09-18 12:52:40 -0400

    e

---
+++ logicoper_nonboolean.js
@@ -0,0 +1,40 @@
+let nr = 0;
+let year = 1970;
+let name = "Alice";
+let empty = "";
+
+console.log(!nr); // -> true
+console.log(!year); // -> false
+console.log(!name); // -> false
+console.log(!empty); // -> true
+
+console.log(!!nr); // -> false
+console.log(!!name); // -> true
+
+// AND operators
+console.log(true && 1991); // -> 1991
+console.log(false && 1991); // -> false
+console.log(2 && 5); // -> 5
+console.log(0 && 5); // -> 0
+console.log("Alice" && "Bob"); // -> Bob
+console.log("" && "Bob"); // -> empty string
+
+// OR operators
+console.log(true || 1991); // -> true
+console.log(false || 1991); // -> 1991
+console.log(2 || 5); // -> 2
+console.log(0 || 5); // -> 5
+console.log("Alice" || "Bob"); // -> Alice
+console.log("" || "Bob"); // -> Bob
+
+// CONTINUED
+let x = 0;
+let y = 0;
+console.log(x++ && y++); // -> 0
+console.log(x); // -> 1
+console.log(y); // -> y == 0
+
+/* Conversely, if the first operand of OR is true, 
+it will be returned, and no other check will be made. 
+This quickens code execution, but has one side effect 
+visible in this example. */
