9d28d3148a556bbe6d387845ddd05e86b6ba9415
aallison06 <131303295+aallison06@users.noreply.github.com>
2023-09-21 10:22:51 -0400

    update iteration

--- notes/iteration.js
+++ notes/iteration.js
@@ -1,19 +1,26 @@
-console.time()
+console.time();
+
+function sleep(miliseconds) { // This function defines a true "sleep" action
+    var currentTime = new Date().getTime();
+ 
+    while (currentTime + miliseconds >= new Date().getTime()) {
+    }
+ }
 
 let x = 70;
 
 function main() {
-    x = x - 1;
+    x -= 1
 }
 
-while(x != 35){
-    if(x != 35){
-        main()
-        console.log(x);
-        console.log("Decreasing the number.");
+while(x != 30){
+    if(x != 30){
+        console.log("Decreasing number", x);
+        main();
+        sleep(1000); // function being declared here
     }
 }
 
-console.log("Number has hit ", x);
-
-console.timeEnd()
+if(x === 30){
+    console.log("Number reached!");
+}
