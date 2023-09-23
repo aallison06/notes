c4bdb45f271b6698ad218a5c90630a6540af213f
aallison06 <131303295+aallison06@users.noreply.github.com>
2023-09-23 13:33:26 -0400

    add lab

--- notes/unit2lab.js
+++ notes/unit2lab.js
@@ -22,6 +22,33 @@ let newContact = {
     email: "risus.Quisque@urna.ca"
 };
 
+let contacts = [{
+name: "Maxwell Wright",
+phone: "(0191) 719 6495",
+email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+}, {
+name: "Raja Villarreal",
+phone: "0866 398 2895",
+email: "posuere.vulputate@sed.com"
+}, {
+name: "Helen Richards",
+phone: "0800 1111",
+email: "libero@convallis.edu"
+}];
+
+let contact = {};
+
+contact.name = prompt("Enter contact name:");
+contact.phone = prompt("Enter contact phone number:");
+contact.email = prompt("Enter contact email:");
+
+contacts.push(contact);
+
+let last = contacts.length - 1;
+
+console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
+
 contacts.push(newContact);
 
 console.log("List of contacts:");
