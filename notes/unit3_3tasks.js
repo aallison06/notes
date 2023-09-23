513f1e2a2411516ce4cb6c1ef40a94d58f00051f
aallison06 <131303295+aallison06@users.noreply.github.com>
2023-09-23 11:40:19 -0400

    add task notes

---
+++ notes/unit3_3tasks.js
@@ -0,0 +1,7 @@
+let width = prompt("Enter the width: ", 0);
+let height = prompt("Enter the height: ", 0);
+let length = prompt("Enter the length: ", 0);
+let box = length * width * height;
+alert(`Calculated box volume: ${box}`);
+
+// Task was to create a volume calculator
